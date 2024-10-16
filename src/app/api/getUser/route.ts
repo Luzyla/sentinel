61dbe085fe0ac1f3f75a503fe5ec8c1525fc092f
Luzyla <luzyla@gmail.com>
2024-10-16 21:45:50 +0200

    Add Nextauth getuser route and  middleware.

---
+++ src/app/api/getUser/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { authOptions } from "../../../../lib/authOptions";
+import { getServerSession } from "next-auth";
+
+export async function GET() {
+    const session = await getServerSession(authOptions);
+
+    if(!session) {
+        return NextResponse.json({error: "Not autorized"}, {status: 400})
+    }
+
+    return NextResponse.json({success: session}, {status: 200})
+};
