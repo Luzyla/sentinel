4ef208e0ef2335eae704028c710c9bf335759680
Luzyla <luzyla@gmail.com>
2024-11-12 01:53:08 +0100

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,11 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/dashboard/(.*)"] }
+export const config = { 
+    matcher: [
+        {
+            source: "/dashboard|list/(.*)",
+            locale: false,
+        }
+    ]
+    
+}
