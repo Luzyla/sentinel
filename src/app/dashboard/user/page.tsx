15f4098e6510d4ff04a1ca5b09bf5a0648b88e54
Luzyla <luzyla@gmail.com>
2024-11-15 23:15:39 +0100

    Remove UserProvider

--- src/app/dashboard/user/page.tsx
+++ src/app/dashboard/user/page.tsx
@@ -1,24 +1,26 @@
-import EventCalendar from "@/components/EventCalendar";
-import Announcements from "@/components/Announcements";
+import EventCalendar from '@/components/EventCalendar';
+import Announcements from '@/components/Announcements';
+import ScheduleCalendar from '@/components/ScheduleCalendar';
 
 const UserPage = () => {
-  return (
-    <div className="p-4 flex gap-4 flex-col xl:flex-row">
-      {/* LEFT */}
-      <div className="w-full xl:w-2/3 flex flex-col bg-[#fafafa44] rounded-xl p-4">
-        <h3 className="text-xl font-semibold">Schedule</h3>
-        {/* bigcalendar */}
-        {/* <div className="h-full p-4 bg- rounded-xl flex gap-4 justify-between flex-wrap">
+	return (
+		<div className="p-4 flex gap-4 flex-col xl:flex-row">
+			{/* LEFT */}
+			<div className="w-full xl:w-2/3 flex flex-col bg-[#fafafa44] rounded-xl p-4">
+				<h3 className="text-xl font-semibold">Schedule</h3>
+				{/* bigcalendar */}
+				<ScheduleCalendar />
+				{/* <div className="h-full p-4 bg- rounded-xl flex gap-4 justify-between flex-wrap">
           
         </div> */}
-      </div>
-      {/* RIGHT */}
-      <div className="w-full xl:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
-        <Announcements />
-      </div>
-    </div>
-  );
+			</div>
+			{/* RIGHT */}
+			<div className="w-full xl:w-1/3 flex flex-col gap-8">
+				<EventCalendar />
+				<Announcements />
+			</div>
+		</div>
+	);
 };
 
 export default UserPage;
