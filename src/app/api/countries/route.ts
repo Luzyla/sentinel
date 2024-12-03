671851a6e817e7754b26a3fa87fdcc5b6004b268
Luzyla <luzyla@gmail.com>
2024-12-03 02:14:53 +0100

    Fix countries issue

--- src/app/api/countries/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET () {
-  const countries = await fetch("https://restcountries.com/v3.1/all");
-  const data = await countries.json();
-
-return NextResponse.json({data}, {status: 200})
-}
